6e74d165eebd1fe2323fa15366d16034090fc1d6
pius.chungath <pius.chungath@graingerglobalonline.com>
2024-07-10 11:46:30 +0100

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as path from "path";
+import * as fs from "fs";
 
 export interface LambdaTrigger {
     type: "apigateway" | "sns";
-    handlerPath: string;
 }
 
 export interface LambdaConfig {
     name: string;
+    handlerFileName: string;
     runtime: aws.lambda.Runtime;
     triggers: LambdaTrigger[];
+    s3Bucket: string;
 }
 
-function createLambdaRole(name: string): aws.iam.Role {
+export function createLambdaRole(name: string): aws.iam.Role {
     const lambdaRole = new aws.iam.Role(`${name}-role`, {
         assumeRolePolicy: {
             Version: "2012-10-17",
@@ -38,15 +40,32 @@ function createLambdaRole(name: string): aws.iam.Role {
     return lambdaRole;
 }
 
+export function uploadLambdaCodeToS3(bucketName: string, handlerFileName: string): aws.s3.BucketObject {
+    const artifactPath = path.resolve(__dirname, "../dist", `${handlerFileName}.zip`);
+    if (!fs.existsSync(artifactPath)) {
+        throw new Error(`Artifact ${artifactPath} not found. Please build the project first.`);
+    }
+
+    const bucket = new aws.s3.Bucket(bucketName);
+
+    return new aws.s3.BucketObject(`${handlerFileName}.zip`, {
+        bucket: bucket,
+        source: new pulumi.asset.FileAsset(artifactPath),
+    });
+}
+
 export function createLambda(config: LambdaConfig): aws.lambda.Function[] {
     const lambdaRole = createLambdaRole(config.name);
     const lambdas: aws.lambda.Function[] = [];
 
+    const bucket = new aws.s3.Bucket(config.s3Bucket);
+
     config.triggers.forEach(trigger => {
+        const s3Object = uploadLambdaCodeToS3(config.s3Bucket, config.handlerFileName);
+
         const lambda = new aws.lambda.Function(`${config.name}-${trigger.type}`, {
-            code: new pulumi.asset.AssetArchive({
-                ".": new pulumi.asset.FileArchive(path.join(__dirname, trigger.handlerPath)),
-            }),
+            s3Bucket: bucket.bucket,
+            s3Key: s3Object.key,
             role: lambdaRole.arn,
             handler: "index.handler",
             runtime: config.runtime,
@@ -98,5 +117,17 @@ export function createLambda(config: LambdaConfig): aws.lambda.Function[] {
     });
 
     return lambdas;
+}
+
+export function createLambdaFromS3(name: string, s3BucketName: string, s3Key: string, handler: string, runtime: aws.lambda.Runtime): aws.lambda.Function {
+    const lambdaRole = createLambdaRole(name);
+
+    return new aws.lambda.Function(name, {
+        s3Bucket: s3BucketName,
+        s3Key: s3Key,
+        role: lambdaRole.arn,
+        handler: handler,
+        runtime: runtime,
+    });
 }
 
